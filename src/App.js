053f67831f97f4ffdeaa39ddd273314f934706f9
Antonio <antonio.mavrin@gmail.com>
2024-12-03 18:22:41 +0100

    + currency converter finished

--- src/App.js
+++ src/App.js
@@ -4,33 +4,48 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [inputCurrency, setInputCurrency] = useState("USD");
   const [outputCurrency, setOutputCurrency] = useState("USD");
-
-  console.log("inputCurrency", inputCurrency);
+  const [error, setError] = useState("");
+  const [rates, setRates] = useState({});
 
   useEffect(
     function () {
       if (!query) return;
 
+      const controller = new AbortController();
+
       async function fetchRate() {
+        setError("");
         try {
           const res = await fetch(
-            `https://api.frankfurter.app/latest?amount=${query}&from=EUR&to=USD`
+            `https://api.frankfurter.app/latest?amount=${query}&from=${inputCurrency}&to=${outputCurrency}`,
+            { signal: controller.signal }
           );
+
+          if (inputCurrency === outputCurrency) {
+            throw new Error(
+              "The input currency and the output currency cannot be the same!"
+            );
+          }
 
           if (!res.ok)
             throw new Error("There was an error while fetching rates!");
 
           const data = await res.json();
 
-          console.log("data", data);
+          setRates(data);
         } catch (err) {
-          console.error(err.message);
+          if (err !== "AbortError") setError(err.message);
         }
       }
 
       fetchRate();
+
+      return function () {
+        controller.abort();
+        setError("");
+      };
     },
-    [query]
+    [query, inputCurrency, outputCurrency]
   );
 
   return (
@@ -40,27 +55,24 @@ export default function App() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      <select>
-        <option value="USD" onChange={(e) => setInputCurrency(e.target.value)}>
-          USD
-        </option>
-        <option value="EUR" onChange={(e) => setInputCurrency(e.target.value)}>
-          EUR
-        </option>
-        <option value="CAD" onChange={(e) => setInputCurrency(e.target.value)}>
-          CAD
-        </option>
-        <option value="INR" onChange={(e) => setInputCurrency(e.target.value)}>
-          INR
-        </option>
-      </select>
-      <select>
+      <select
+        onChange={(e) => {
+          setInputCurrency(e.target.value);
+        }}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <select onChange={(e) => setOutputCurrency(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{error ? error : ""}</p>
+      <p>{!error && rates ? rates.rates[outputCurrency] : ""}</p>
     </div>
   );
 }
